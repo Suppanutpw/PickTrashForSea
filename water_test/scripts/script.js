84109186f5c26174a7137ee151ac82446e1d83c8
Suppanutpw <54876422+Suppanutpw@users.noreply.github.com>
2020-02-21 22:10:50 +0700

    add timecounter, score result

--- water_test/scripts/script.js
+++ water_test/scripts/script.js
@@ -5,11 +5,19 @@ const Patches = require('Patches');
 // Use export keyword to make a symbol available in scripting debug console
 export const Diagnostics = require('Diagnostics');
 
+// Define Text
 var counterNumber = Scene.root.find('showscore');
 var counterTime = Scene.root.find('timecounter');
+var gameCounter = Scene.root.find('gamecounter');
+var result = Scene.root.find('result');
 
+// Define Variable from Patches
 var score = Patches.getScalarValue('score');
 var time = Patches.getScalarValue('time');
+var gameTime = Patches.getScalarValue('gametime');
 
+// Output via Text
 counterNumber.text = score.toString();
 counterTime.text = time.toString();
+gameCounter.text = gameTime.toString();
+result.text = score.toString();
